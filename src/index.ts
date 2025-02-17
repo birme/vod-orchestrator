a5417c022fd66089064285ed99e1436b6eb92dfa
Jonas Birmé <jonas.birme@eyevinn.se>
2025-02-17 08:53:53 +0100

    feat: save asset to database

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { Context } from '@osaas/client-core';
 import { createVod, createVodPipeline } from '@osaas/client-transcode';
 import http from 'node:http';
 import { setupListener } from './listener';
+import { setupDatabase } from '@osaas/client-db';
+import nano from 'nano';
+
+interface Asset extends nano.MaybeDocument {
+  assetId: string,
+  vodUrl: string
+};
 
 async function main() {
   const ctx = new Context();
@@ -15,6 +22,15 @@ async function main() {
     const pipeline = await createVodPipeline('vodsvc', ctx, { createInputBucket: true });
     console.log(pipeline);
 
+    const dbUrl = await setupDatabase('couchdb', 'vodsvc', { rootPassword: 'verysecret' });
+    console.log(dbUrl);
+    const client = nano(dbUrl.toString());
+    const dbList = await client.db.list();
+    if (!dbList.includes('myassets')) {
+      await client.db.create('myassets');
+    }
+    const db = client.use('myassets');
+
     if (pipeline.inputStorage) {
       setupListener(
         pipeline.inputStorage.name,
@@ -26,6 +42,12 @@ async function main() {
           const sourceUrl = new URL(`s3://${record.s3.bucket.name}/${record.s3.object.key}`);
           const vod = await createVod(pipeline, sourceUrl.toString(), ctx);
           console.log(vod);
+          const asset: Asset = {
+            assetId: vod.id,
+            vodUrl: vod.vodUrl
+          };
+          await db.insert(asset);
+          console.log('Asset saved to database');
         });
     } else {
       console.error('No input storage found, failed to setup listener');
